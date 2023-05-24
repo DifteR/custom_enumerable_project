0ef1c6053f2e82052f9388bd27465debbdc25322
DifteR <its0black0magic@gmail.com>
2023-05-24 09:43:59 +0200

    Add my_each and my_each_with_index

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,4 +8,21 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each()
+    i = 0
+    while i < self.length
+      yield(self[i])
+      i += 1
+    end
+    return self
+  end
+
+  def my_each_with_index()
+    i = 0
+    while i < self.length
+      yield(self[i], i)
+      i += 1
+    end
+    return self
+  end
 end
